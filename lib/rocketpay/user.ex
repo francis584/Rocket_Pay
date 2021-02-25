659d8248d1363c513bb0e867a2a8a85b5326ad4c
Francinildo M. Rodrigues <francis584@gmail.com>
2021-02-25 06:52:01 -0300

    create account using Multi

--- lib/rocketpay/user.ex
+++ lib/rocketpay/user.ex
@@ -2,6 +2,7 @@ defmodule Rocketpay.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Rocketpay.Account
   alias Ecto.Changeset
 
   @primary_key {:id, :binary_id, autogenerate: true}
@@ -15,6 +16,7 @@ defmodule Rocketpay.User do
     field :password, :string, virtual: true
     field :password_hash, :string
     field :nickname, :string
+    has_one :account, Account
 
     timestamps()
   end
@@ -23,7 +25,7 @@ defmodule Rocketpay.User do
     %__MODULE__{}
     |> cast(params, @required_params)
     |> validate_required(@required_params)
-    |> validate_length(:password_hash, min: 6)
+    |> validate_length(:password, min: 6)
     |> validate_number(:age, greater_than_or_equal_to: 18)
     |> validate_format(:email, ~r/@/)
     |> unique_constraint([:email])
